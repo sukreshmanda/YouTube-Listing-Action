b6ba280f7559724dbcd988e3795840aaf75bda2d
sukreshmanda <34400639+sukreshmanda@users.noreply.github.com>
2020-12-04 10:29:23 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -6,10 +6,12 @@ fs.readFile('list.conf', 'utf8' , function (err,data) {
   }
   let array = data.split(os.EOL)
   let i=0
+  let returning = ""
   for(i;i<array.length;i++){
     let data = array[i]
     let text = data.substring(0,data.indexOf("https://"))
     let link = data.substring(data.indexOf("https://"),data.length)
-    console.log("text: "+text+" link: "+link)
+    let source = "<img src=\"https://cdn1.iconfinder.com/data/icons/web-page-and-iternet/90/Web_page_and_internet_icons-04-512.png\" width=\"22px\"> <a href=\""+link+"\">"+text+" </a> <br />"
+    console.log(source)
   }
 });
